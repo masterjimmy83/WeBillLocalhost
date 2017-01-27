482d27745afc9e783143d05325d210a48adf57e4
master <master@localhost>
2017-01-27 20:09:35 +0900

    Tried to make auto generation of userID, but realized it is not supposed to be in this form, it will be done when inserting new user.

--- src/java/cleanenergy/webill/validate.java
+++ src/java/cleanenergy/webill/validate.java
@@ -66,136 +66,135 @@ public class validate extends HttpServlet {
             }
             md5Secret = strBuilder.toString();
             if (!userID.isEmpty() && !secret.isEmpty() && !userRole.isEmpty()) {
-                if ("Admin".equals(userRole)){
-                String idSecretMatchQuery = "select * from admins where userName='" + userID + "'and password ='" + md5Secret + "' and role ='" + userRole + "';";
-                //select * from admins where userName ='admin-001' and password='df76807c147cb7e4348b55dd4e6cb48e'and role='Admin';
-                ResultSet matchingUser = sqlConnection.createStatement().executeQuery(idSecretMatchQuery);
-                if (matchingUser.next()) {
+                if ("Admin".equals(userRole)) {
+                    String idSecretMatchQuery = "select * from admins where userName='" + userID + "'and password ='" + md5Secret + "' and role ='" + userRole + "';";
+                    //select * from admins where userName ='admin-001' and password='df76807c147cb7e4348b55dd4e6cb48e'and role='Admin';
+                    ResultSet matchingUser = sqlConnection.createStatement().executeQuery(idSecretMatchQuery);
+                    if (matchingUser.next()) {
 
-                    //Here we found a matching user in the DB
-                    //Save the given name in the session.
-                    //session.setAttribute("userName", matchingUser.getString("givenName"));
-                    //out.println("Welcome " + matchingUser.getString("givenName") + " " + matchingUser.getString("SurName"));
-                    //response.sendRedirect("web/fileUpload.html");
-                    switch (userRole) {
-                        case "Technician": {
-                            session.setAttribute("userName", matchingUser.getString("userName"));
-                            if (session.getAttribute("userName") == null) {
-                                response.sendRedirect("index.html");
-                            } else {
-                                RequestDispatcher rd = request.getRequestDispatcher("fileUpload.html");
-                                rd.forward(request, response);}
+                        //Here we found a matching user in the DB
+                        //Save the given name in the session.
+                        //session.setAttribute("userName", matchingUser.getString("givenName"));
+                        //out.println("Welcome " + matchingUser.getString("givenName") + " " + matchingUser.getString("SurName"));
+                        //response.sendRedirect("web/fileUpload.html");
+                        switch (userRole) {
+                            case "Technician": {
+                                session.setAttribute("userName", matchingUser.getString("userName"));
+                                if (session.getAttribute("userName") == null) {
+                                    response.sendRedirect("index.html");
+                                } else {
+                                    RequestDispatcher rd = request.getRequestDispatcher("fileUpload.html");
+                                    rd.forward(request, response);
+                                }
                                 break;
                             }
-                        
-                    case "Admin":
-                            {
+
+                            case "Admin": {
                                 session.setAttribute("userName", matchingUser.getString("userName"));
                                 RequestDispatcher rd = request.getRequestDispatcher("admin");
                                 rd.forward(request, response);
                                 break;
                             }
-                        default:
-                            {
+                            default: {
                                 RequestDispatcher rd = request.getRequestDispatcher("index.html");
                                 rd.forward(request, response);
                                 break;
                             }
+                        }
+
+                    } else {
+                        // Mean the credential given are not correct, so send back to login page
+                        response.sendRedirect("index.html");
+
+                    }
+                }
+                if ("Customer".equals(userRole)) {
+                    String idSecretMatchQuery = "select * from users where userID='" + userID + "'and secret ='" + md5Secret + "' and userRole ='" + userRole + "';";
+
+//String idSecretMatchQuery = "SELECT CONCAT('user-', userID), secret, userRole FROM customers WHERE CONCAT('user-',userID) = '" + userID + "'and secret ='" + md5Secret + "' and userRole ='" + userRole + "';";
+                    //String idSecretMatchQuery = "SELECT CONCAT('user-', userID), secret, userRole FROM customers WHERE CONCAT('user-',userID) = "+userID+" and secret="+md5Secret+" and userRole="+userRole+";";
+                    ResultSet matchingUser = sqlConnection.createStatement().executeQuery(idSecretMatchQuery);
+                    if (matchingUser.next()) {
+
+                        //Here we found a matching user in the DB
+                        //Save the given name in the session.
+                        //session.setAttribute("userName", matchingUser.getString("givenName"));
+                        //out.println("Welcome " + matchingUser.getString("givenName") + " " + matchingUser.getString("SurName"));
+                        //response.sendRedirect("web/fileUpload.html");
+                        session.setAttribute("userName", matchingUser.getString("givenName"));
+                        if (session.getAttribute("userName") == null) {
+                            response.sendRedirect("index.html");
+                        } else {
+                            RequestDispatcher rd = request.getRequestDispatcher("fileUpload.html");
+                            rd.forward(request, response);
+                        }
+
                     }
 
-                } else {
-                    // Mean the credential given are not correct, so send back to login page
-                    response.sendRedirect("index.html");
-
-                }
-                }
-              if ("Customer".equals(userRole)){ 
-                  String idSecretMatchQuery = "select * from users where userID='" + userID + "'and secret ='" + md5Secret + "' and userRole ='" + userRole + "';";
-                ResultSet matchingUser = sqlConnection.createStatement().executeQuery(idSecretMatchQuery);
-                if (matchingUser.next()) {
-
-                    //Here we found a matching user in the DB
-                    //Save the given name in the session.
-                    //session.setAttribute("userName", matchingUser.getString("givenName"));
-                    //out.println("Welcome " + matchingUser.getString("givenName") + " " + matchingUser.getString("SurName"));
-                    //response.sendRedirect("web/fileUpload.html");
-                            session.setAttribute("userName", matchingUser.getString("givenName"));
-                            if (session.getAttribute("userName") == null) {
-                                response.sendRedirect("index.html");
-                            } else {
-                                RequestDispatcher rd = request.getRequestDispatcher("fileUpload.html");
-                                rd.forward(request, response);}
-              
-              }
-                
                 }
             }
 
-            }catch (NoSuchAlgorithmException | SQLException ex) {
+        } catch (NoSuchAlgorithmException | SQLException ex) {
             //Logger.getLogger(validate.class.getName()).log(Level.SEVERE, null, ex);
             //System.err.println("Error while getting the hashcode:"+ex.getMessage());
             System.err.println("Error in the Servlet");
             System.err.println(ex.getMessage());
         }
-        }
+    }
 
-        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-        /**
-         * Handles the HTTP <code>GET</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doGet
-        (HttpServletRequest request, HttpServletResponse response)
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            processRequest(request, response);
-        }
+        processRequest(request, response);
+    }
 
-        /**
-         * Handles the HTTP <code>POST</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doPost
-        (HttpServletRequest request, HttpServletResponse response)
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            processRequest(request, response);
-        }
+        processRequest(request, response);
+    }
 
-        /**
-         * Returns a short description of the servlet.
-         *
-         * @return a String containing servlet description
-         */
-        @Override
-        public String getServletInfo
-        
-            () {
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
         return "Short description";
-        }// </editor-fold>
+    }// </editor-fold>
 
-        @Override
-        public void init() throws ServletException {
-            super.init();
-            if (sqlConnection == null) {
-                System.out.println("Establish a new Connection to the database.....");
-                try {
-                    Class.forName("com.mysql.jdbc.Driver").newInstance();
-                    //sqlConnection = DriverManager.getConnection("jdbc:mysql://localhost/WeBill?" + "user=webill&password=itsme&useSSL=false");
-                    sqlConnection = DriverManager.getConnection("jdbc:mysql://localhost/WeBill?" + "user=webill&password=itsme&useSSL=false");
-                    System.out.println("The Connection was Successful!");
-                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
-                    System.err.println("Error in establishing the connection to the database! Details follows");
-                    System.err.println(ex.toString());
-                }
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        if (sqlConnection == null) {
+            System.out.println("Establish a new Connection to the database.....");
+            try {
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                //sqlConnection = DriverManager.getConnection("jdbc:mysql://localhost/WeBill?" + "user=webill&password=itsme&useSSL=false");
+                sqlConnection = DriverManager.getConnection("jdbc:mysql://localhost/WeBill?" + "user=webill&password=itsme&useSSL=false");
+                System.out.println("The Connection was Successful!");
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+                System.err.println("Error in establishing the connection to the database! Details follows");
+                System.err.println(ex.toString());
             }
         }
     }
+}
